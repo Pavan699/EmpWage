5128bad82f9163b00f4f1944a46f8f6e4459ebef
Pavan699 <pavannakate03@gmail.com>
2021-09-09 06:17:52 -0700

    Adding Title

---
+++ Emp_Wage_Multiple_Wage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emp_Wage_Multiple_Wage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiple Company Wages ::");
+        }
+    }
+}
