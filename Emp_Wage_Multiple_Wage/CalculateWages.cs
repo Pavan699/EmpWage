63762bee9d8f6b9bac9d1a11650720e4a97a2afc
Pavan699 <pavannakate03@gmail.com>
2021-09-09 08:02:41 -0700

    Adding Comments to Program

--- Emp_Wage_Multiple_Wage/CalculateWages.cs
+++ Emp_Wage_Multiple_Wage/CalculateWages.cs
@@ -5,18 +5,21 @@ using System.Text;
 namespace Emp_Wage_Multiple_Wage
 {
     class CalculateWages
-    {
-        int oneDay_Sal = 0;
-        int perHour_Pay = 20;        
-        int perhour;
+    {   
+        //Class Variables
+        public int oneDay_Sal = 0;
+        public int perHour_Pay = 20;        
+        public int perhour;       
+        public int tot_Wage = 0;      
         
-        int tot_Wage = 0;      
-        
+        //Inbuild Random Function Object creation 
         Random emp_random = new Random();
-        public void check(string cmp_name,int cmp_num, int max_WorkDay ,int tot_MaxHours)
+        //check method for implimentation
+        public void check(string cmp_name, int cmp_num, int max_WorkDay , int tot_MaxHours)
         {
             int tot_Workhours = 0;
             int day = 1;
+            //While for condition Days and Hours for each Company 
             while (day <= max_WorkDay && tot_Workhours <= tot_MaxHours)
             {
                 int empcheck = emp_random.Next(0, 3);
@@ -33,11 +36,12 @@ namespace Emp_Wage_Multiple_Wage
                         break;
                 }
                 day++;
-                oneDay_Sal = perhour * perHour_Pay;               
+                oneDay_Sal = perhour * perHour_Pay;                
                 tot_Workhours = tot_Workhours + perhour;
             }
             Console.WriteLine();
             tot_Wage = tot_Workhours * perHour_Pay;
+            //Displaying the Wages And Information for Comapnies
             Console.WriteLine("Comapny Name is : " + cmp_name);
             Console.WriteLine("Comapny Number is : " + cmp_num);
             Console.WriteLine("Total Wage  For Month is : " + tot_Wage);
--- Emp_Wage_Multiple_Wage/Program.cs
+++ Emp_Wage_Multiple_Wage/Program.cs
@@ -2,13 +2,16 @@
 
 namespace Emp_Wage_Multiple_Wage
 {
+    // Main Class
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Multiple Company Wages ::");
+            //Object for CalculateWages class 
             CalculateWages cal = new CalculateWages();
-            cal.check("Bridge-Lab", 144, 24, 100);
+            //calling check method
+            cal.check("Bridge-Lab", 144, 24, 100); //("Company Name", Company_Number, Work_Days, Work_Hours)
             cal.check("Capgi", 234, 20, 80);
             cal.check("TATA", 001, 26, 120);
             cal.check("Wipro", 004, 22, 105);
