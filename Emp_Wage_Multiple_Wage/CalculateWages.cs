8c16e266e5169dfac110bbe5392cd99f8ab16ce9
Pavan699 <pavannakate03@gmail.com>
2021-09-09 07:08:40 -0700

    Condition for 100 hours and 20 days

--- Emp_Wage_Multiple_Wage/CalculateWages.cs
+++ Emp_Wage_Multiple_Wage/CalculateWages.cs
@@ -12,12 +12,13 @@ namespace Emp_Wage_Multiple_Wage
         int perhour;
         int tot_Workhours = 0;
         int tot_Wage = 0;
+        int tot_MaxHours = 100;
         
         Random emp_random = new Random();
         public void check()
         {
             int day = 1;
-            while (day <= max_WorkDay)
+            while (day <= max_WorkDay && tot_Workhours <= tot_MaxHours)
             {
                 int empcheck = emp_random.Next(0, 3);
                 switch (empcheck)
@@ -34,10 +35,13 @@ namespace Emp_Wage_Multiple_Wage
                 }
                 day++;
                 oneDay_Sal = perhour * perHour_Pay;
+                Console.Write( oneDay_Sal + "  ");
                 tot_Workhours = tot_Workhours + perhour;
             }
+            Console.WriteLine();
             tot_Wage = tot_Workhours * perHour_Pay;
-            Console.WriteLine("Total Wage For Month is : " + tot_Wage);
+            Console.WriteLine("Total Wage  For Month is : " + tot_Wage);
+            Console.WriteLine("Total Hours For Month is : " + tot_Workhours);
         }
     }
 }
